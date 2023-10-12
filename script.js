ba31ed9b23c9c1cfde7c60bbf4b70a4247a56427
mawethuN <117039415+mawethuN@users.noreply.github.com>
2023-10-12 18:14:20 +0200

    Submission 3
    The following iterations were made regarding feedback received from submission 2:
    >Removal of unnecessary divs and br
    >fixing headings on each page
    >writing attributes for all images
    >fixing navigation consistency for each page
    >Making navbar visible
    >removal of unnecessary buttons
    >fixing background colour
    >redone wireframes/incl new wireframes
    >elaboration on design choices
    >implementing the usage of whitespace
    >Introduction to each page
    >simplifying home page
    >adding an interactive graph
    >updating blog post/adding blog post 3
    >updating design process
    >adding something to the data art section
    >additional CSS tweaking

--- script.js
+++ script.js
@@ -238,3 +238,52 @@ fetch(apiUrl)
     createRedBarGraph(processedData);
   })
   .catch(error => console.error("Error fetching data:", error));
+
+  function animateBars(data) {
+    var svg = d3.select(methods.el["selector"] + " .barchart");
+    var barrects = d3.select(methods.el["selector"] + " .barrects");
+  
+    var initialHeight = 0;
+  
+    var bar = barrects.selectAll("rect")
+      .data(data);
+  
+    // Enter
+    var enteredBars = bar.enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("y", initialHeight);
+  
+    // Update and animate the bars
+    bar
+      .attr("height", initialHeight)
+      .transition()
+      .duration(500)
+      .attr("x", function (d) { return methods.x(d.letter); })
+      .attr("width", methods.x.rangeBand())
+      .attr("y", initialHeight) // Start with a height of 0
+      .attr("height", 0) // Start with a height of 0
+      .transition() // Animate the height
+      .duration(1000) // Animation duration
+      .attr("y", function (d) { return methods.y(d.frequency); })
+      .attr("height", function (d) { return methods.height - methods.y(d.frequency); });
+  
+    // Add a hover option to display coordinates
+    enteredBars
+      .on("mouseover", function (d) {
+        d3.select(this)
+          .append("title")
+          .text(function (d) { return "Letter: " + d.letter + "\nFrequency: " + d.frequency; });
+      })
+      .on("mouseout", function () {
+        d3.select(this).select("title").remove();
+      });
+  
+    // Exit and remove any bars not in the updated data
+    bar.exit()
+      .transition()
+      .duration(250)
+      .attr("y", initialHeight)
+      .attr("height", initialHeight)
+      .remove();
+  }
